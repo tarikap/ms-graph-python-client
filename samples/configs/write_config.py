64a2df6f6c1bfcd89a78a500fb4bd04b560a5232
tarikap <tarik.kapkovic@gmail.com>
2020-12-06 12:15:53 +0100

    config change

--- samples/configs/write_config.py
+++ samples/configs/write_config.py
@@ -7,9 +7,9 @@ config = ConfigParser()
 config.add_section('graph_api')
 
 # Set the Values.
-config.set('graph_api', 'client_id', '')
-config.set('graph_api', 'client_secret', '')
-config.set('graph_api', 'redirect_uri', '')
+config.set('graph_api', 'client_id', '976cc237-b711-47f9-980c-4f12b6fb5d96')
+config.set('graph_api', 'client_secret', '6zdc326a39s4KWg9SjMV~Rci2~wO-U.vYn')
+config.set('graph_api', 'redirect_uri', 'https://localhost/todo_login')
 
 # Write the file.
 with open(file='samples/configs/config.ini', mode='w+') as f:
---
+++ samples/test_py.py
@@ -0,0 +1,4 @@
+from pprint import pprint
+from configparser import ConfigParser
+from ms_graph.client import MicrosoftGraphClient
+
